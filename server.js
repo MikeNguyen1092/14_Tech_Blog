ea29aeb60d0db956fb5efed8e71c5367754dbce5
Mike <nguyen.mike1092@gmail.com>
2024-02-07 23:26:50 -0500

    trying heroku again

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const helpers = require("./utils/helpers");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({ helpers });
+
 const sess = {
 	secret: "Super secret secret",
 	cookie: { maxAge: 300000, httpOnly: true, secure: false, sameSite: "strict" },
@@ -21,9 +23,9 @@ const sess = {
 	}),
 };
 
-app.use(session(sess));
 
-const hbs = exphbs.create({ helpers });
+
+app.use(session(sess));
 
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
