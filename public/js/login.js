f2d61354fcc49f14ea5edc1e329ee3d59a0caba5
Mike <nguyen.mike1092@gmail.com>
2024-02-07 21:10:37 -0500

    logged out message for incorrect pass

--- public/js/login.js
+++ public/js/login.js
@@ -5,16 +5,17 @@ const loginFormHandler = async (event) => {
 	const password = document.querySelector("#password-login").value.trim();
 
 	if (email && password) {
-			const response = await fetch("/api/users/login", {
-				method: "POST",
-				body: JSON.stringify({ email, password }),
-				headers: { "Content-Type": "application/json" },
-			});
-			if (response.ok) {
-				document.location.replace("/");
-			} else {
-				alert("Failed to log in.");
-			}
+		const response = await fetch("/api/users/login", {
+			method: "POST",
+			body: JSON.stringify({ email, password }),
+			headers: { "Content-Type": "application/json" },
+		});
+		if (response.ok) {
+			document.location.replace("/");
+		} else {
+			const responseBody = await response.json();
+			alert(responseBody.message || "Failed to log in.");
+		}
 	}
 };
 
